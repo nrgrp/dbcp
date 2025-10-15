15a8496b35de46c5c5a24aae527256a73832806f
Hao Zhu <zhuh@cs.uni-freiburg.de>
2025-10-15 13:55:39 +0200

    add __init__.py

--- src/dbcp/__init__.py
+++ src/dbcp/__init__.py
@@ -1,1 +1,9 @@
-from dbcp.problem import BiconvexProblem
+from .problem import BiconvexProblem
+
+__all__ = ["BiconvexProblem", "__version__"]
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    __version__ = version("dbcp")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
